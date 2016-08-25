a8a39548f4eac95043e8efb8d23d15bce92036a9
felipecaetanosilva <felipecaetanosilva@hotmail.com>
2016-08-25 01:47:56 -0300

    Consegue ler os comandos ls e date

--- ep1sh.c
+++ ep1sh.c
@@ -8,41 +8,59 @@
 # define MAX_LENGTH 1024
 
 static char *line = (char *) NULL;
-char *command = {"/bin/ls"};
-char *argument[] = {"/bin/ls", "-l", NULL };
+char *command;
+char *argument[5];
 
 
 void type_prompt ();
+int splitter (char line[], char *dic[]);
 
 void type_prompt () {
-	char cwd[MAX_LENGTH];
-	getcwd (cwd, sizeof (cwd));
-	strcat (cwd, ": ");
-	if (line) {
-		free (line);
-		line = (char *) NULL;
-	}
-	line = readline (cwd);
-	if (line && *line)
-		add_history (line);
+    int n;
+    char cwd[MAX_LENGTH];
+    char *dir;
+    dir = getcwd (cwd, sizeof (cwd));
+    if (dir == NULL) printf ("Erro no diretorio! Cuidado!");
+    strcat (cwd, ": ");
+    if (line) {
+        free (line);
+        line = (char *) NULL;
+    }
+    line = readline (cwd);
+    if (line && *line) {
+        add_history (line);
+        n = splitter (line, argument);
+        argument[n] = NULL;
+        command = argument[0];
+    }
+
 }
 
-
+int splitter (char line[], char *dic[]) {
+    char *token;
+    int n = 0;
+    token = strtok (line, " ");
+    while (token != NULL) {
+        dic[n++] = token;
+        token = strtok (NULL, " ");
+    }
+    return n;
+ }
 
 int main (int argc, char * argv[]) {
-	int e;
 
-	while(1) {
-		type_prompt ();
-		if (fork () != 0) {
-			wait(0);
-			type_prompt ();
-		}
-		else {
-			e = execve(command, argument, NULL);
-			printf("%d\n", e);
-		}
+    int e, pid;
 
-	}
-	return EXIT_SUCCESS;
+    while(1) {
+        type_prompt ();
+        if ((pid = fork ()) != 0) {
+            wait(0);
+        }
+        else {
+            e = execve(command, argument, NULL);
+            printf("%d\n", e);
+        }
+
+    }
+    return EXIT_SUCCESS;
 }
