7204c87fab9bb9406af3c0b4c70ca7a2d67b6c39
Angelo Gregorio Lovatto <angelolovatto@gmail.com>
2016-09-01 21:02:32 -0300

    gambiarra do FCFS funcionando

--- ep1.c
+++ ep1.c
@@ -1,7 +1,8 @@
+/* Compilar com gcc -o ep1 ep1.c -Wall -std=c99 -ansi -pedantic -pthread */
 # include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <unist.h>
+# include <unistd.h>
 
 
 # define QUANTUM 1
@@ -13,8 +14,15 @@ typedef struct {
     double deadline;
 } processo;
 
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+    void *(*start_routine)(void*), void *arg);
+int pthread_join(pthread_t thread, void **value_ptr);
+void* lostimeFCFS (void* voidtime);
+void FCFS ();
+void redimensiona ();
+void readFile ();
 
-
+    
 FILE *in;
 FILE *out;
 processo *listaproc;
@@ -24,35 +32,37 @@ pthread_mutex_t mutex;
 
 void FCFS () {
 	int i;
-	clock_t tf, tini;
-	pthread_t procs[];
+	clock_t tf;
+	pthread_t *procs;
 	pthread_mutex_init(&mutex, NULL);
 
 	procs = malloc (sizeof (pthread_t) * size);
 
 	for (i = 0; i < size; i++) {
-		pthread_create (&procs[i], NULL, lostimeFCFS, listaproc[i].dt);
-		pthread_join(procs[i], NULL);
-		tf = clock ();
-		fprintf (out, "%s", listaproc[i].nome);
-		fprintf(out, " %lf", tf);
-		fprintf(out, "%lf\n", tf - listaproc[i].t0);
+            pthread_create (&procs[i], NULL, lostimeFCFS, &listaproc[i].dt);
+            pthread_join(procs[i], NULL);
+            tf = clock ();
+            fprintf (out, "%s", listaproc[i].nome);
+            fprintf(out, " %Lf", (long double) tf);
+            fprintf(out, " %Lf\n", (long double) tf - listaproc[i].t0);
 	}
 
-	pthread_exit (NULL);
 	pthread_mutex_destroy(&mutex);
 	    
 }
 
-void lostimeFCFS (double time) {
+void* lostimeFCFS (void *voidtime) {
 	double i = 0;
+        double *time = (double *)voidtime;
 
-	while (i < time) {
+	while (i < *time) {
 		pthread_mutex_lock (&mutex);
 		sleep (QUANTUM);
-		i = i + QUANTUM;
+		i += QUANTUM;
 		pthread_mutex_unlock (&mutex);
 	}
+
+        return NULL;
 }
 
 void redimensiona () {
@@ -63,7 +73,7 @@ void redimensiona () {
 void readFile () {
     listaproc = malloc (N * sizeof (processo));
     
-    while (fscanf (in, "%lf %s %lf %lf", &listaproc[size].t0, listaproc[size].nome, &listaproc[size].dt, &listaproc[size].deadline) != 0) {
+    while (fscanf (in, "%lf %s %lf %lf", &listaproc[size].t0, listaproc[size].nome, &listaproc[size].dt, &listaproc[size].deadline) != EOF) {
         size++;
         if (size == N) redimensiona ();
     }
