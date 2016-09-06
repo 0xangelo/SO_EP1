414066e3d398057121fd594fadee1c2b9e426f6e
Felipe Caetano Silva <fcaetano@bigdaddy.linux.ime.usp.br>
2016-09-06 17:02:32 -0300

    Escalonador SRTN aparentemente pronto

--- ep1.c
+++ ep1.c
@@ -5,7 +5,6 @@
 # include <unistd.h>
 # include <sched.h>
 
-# define QUANTUM 1
 # define _GNU_SOURCE
 
 typedef struct {
@@ -21,11 +20,14 @@ int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
 int pthread_join(pthread_t thread, void **value_ptr);
 
 
-void* lostimeFCFS (void* voidtime);
+void* lostime (void* voidtime);
 void FCFS ();
+
+
 void redimensiona ();
 void readFile ();
-void errentrada (processo a);
+
+void SRTN ();
 
     
 FILE *in;
@@ -36,17 +38,8 @@ int N = 2;
 int size = 0;
 int cpu;
 int currenti = 0;
-pthread_mutex_t mutex;
+int shortest = 0;
 
-void errentrada (processo a) {
-    fprintf (stderr, "Linha de Trace: %f %s %f %f\n", a.t0, a.nome, a.dt, a.deadline);
-}
-void errsaida (char *nome, double final, double relogio, int i) {
-    fprintf(out, "%s %.2f(s) %.2f(s)\n", nome, final , relogio);
-    fprintf(stderr, "Linha de Saida: %s %f %f\n", nome, final, relogio);
-    if (i == 1)
-        fprintf(stderr, "Número de mudanças de Contexto: 0\n");
-}
 
 void* arrival (void *time) {
     int i = 0;
@@ -84,7 +77,7 @@ void FCFS () {
             ops = (clock () - begin) / CLOCKS_PER_SEC;
             c = (double) ops;
             if (c >= listaproc[i].t0) {
-                pthread_create (&procs[i], NULL, lostimeFCFS, &listaproc[i].dt);
+                pthread_create (&procs[i], NULL, lostime, &listaproc[i].dt);
                 pthread_join(procs[i], NULL);
                 end = clock ();
                 tf = (double) (end - begin)/CLOCKS_PER_SEC;
@@ -103,17 +96,6 @@ void FCFS () {
 	    
 }
 
-void* lostimeFCFS (void *voidtime) {
-    double *time = (double *)voidtime;
-    clock_t begin = clock ();
-    cpu = sched_getcpu ();
-    if (d) fprintf (stderr, "  %s começou a usar CPU %d\n", listaproc[currenti].nome, cpu);
-
-	while (((double) (clock () - begin)/CLOCKS_PER_SEC) < *time) {}
-
-    if (d) fprintf (stderr, "     %s terminou de usar CPU %d\n", listaproc[currenti].nome, cpu);
-    return NULL;
-}
 
 void redimensiona () {
     N *= 2;
@@ -129,6 +111,68 @@ void readFile () {
     }
 
 }
+void* lostime (void *voidtime) {
+    double *time = (double *)voidtime;
+    clock_t begin = clock ();
+    cpu = sched_getcpu ();
+    if (d) fprintf (stderr, "  %s começou a usar CPU %d\n", listaproc[shortest].nome, cpu);
+
+    while (((double) (clock () - begin)/CLOCKS_PER_SEC) < *time) {}
+    if (d-1) fprintf (stderr, "  %s terminou de usar CPU %d\n", listaproc[shortest].nome, cpu);
+    return NULL;
+}
+
+void SRTN () {
+    double QUANTUM;
+    int i = 0, j, contexto = 0, flag = 0;
+    clock_t begin, end;
+    double tf;
+    pthread_t run, time;
+    QUANTUM = 0.1;
+    begin = clock ();
+
+    if (d) pthread_create (&time, NULL, arrival, &begin);
+
+    while (i < size) {
+        while (1) {
+            if (listaproc[shortest].dt > 0) {
+                if ((double) ((clock () - begin)/CLOCKS_PER_SEC) >= listaproc[shortest].t0)
+                    break;
+            }
+            else shortest++;
+        }
+        flag = 0;
+
+        for (j = 0; j < size; j++) {
+            if ((listaproc[j].t0 <= (double) ((clock () - begin)/CLOCKS_PER_SEC)) && 
+                (listaproc[j].dt < listaproc[shortest].dt) && 
+                (listaproc[j].dt > 0)) {
+
+                shortest = j;
+                flag = 1;
+            }
+        }
+        if (flag) contexto++;
+        if (listaproc[shortest].dt > 0) {
+            pthread_create (&run, NULL, lostime, &QUANTUM);
+            pthread_join (run, NULL);
+            listaproc[shortest].dt = listaproc[shortest].dt - QUANTUM;
+            if (listaproc[shortest].dt <= 0) {
+                end = clock ();
+                tf = (double) (end - begin)/CLOCKS_PER_SEC;
+
+                fprintf(out, "%s %f %f\n", listaproc[shortest].nome, tf, tf - listaproc[shortest].t0);
+            
+                if (d) fprintf(stderr, "Fim do %s : %s %f %f\n", listaproc[shortest].nome, listaproc[shortest].nome, tf, tf - listaproc[shortest].t0);
+                i++;
+                shortest = 0;
+            }
+        }
+    }
+    fprintf(out, "Mudanças de Contexto: %d\n", contexto);
+    if (d) fprintf (stderr, "Terminado. Número de mudanças de Contexto: %d\n", contexto);
+
+}
 
 int main (int argc, char **argv) {
     int escalonador = atoi (argv[1]);
@@ -140,9 +184,14 @@ int main (int argc, char **argv) {
 
     readFile ();
 
+
     switch (escalonador) {
         case 1:
             FCFS ();
+            break;
+        case 2:
+            SRTN ();
+            break;
     }
     return 0;
 }
