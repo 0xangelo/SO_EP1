9f001cf07ab9659ddc3ce275862acdefd98aae65
Felipe Caetano Silva <fcaetano@lisp.linux.ime.usp.br>
2016-09-07 20:09:09 -0300

    Multiplas Filas feito

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-CFLAGS= -Wall -std=c99 -ansi -pedantic -O2 -lreadline
+CFLAGS= -Wall -std=c99 -ansi -pedantic -O2 -lreadline -lpthread
 CC= gcc
+
+ep1: ep1.c
+	${CC} -o $@ $^ ${CFLAGS}
 
 ep1sh: ep1sh.c
 	${CC} -o $@ $^ ${CFLAGS}
--- ep1.c
+++ ep1.c
@@ -20,7 +20,10 @@ int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
 int pthread_join(pthread_t thread, void **value_ptr);
 
 
-void* lostime (void* voidtime);
+void * lostime (void* voidtime);
+void * arrival (void *time);
+
+
 void FCFS ();
 
 
@@ -29,17 +32,199 @@ void readFile ();
 
 void SRTN ();
 
-    
+
+void MULTIPLAS_FILAS ();
+void * arrivalM (void *time);
+int tem_alguem (int p, int u);
+void daumplus (int m);
+
 FILE *in;
 FILE *out;
 processo *listaproc;
+int **filas;
+int *p, *u;
 int d = 0;
 int N = 2;
 int size = 0;
 int cpu;
 int currenti = 0;
-int shortest = 0;
+int escalonador;
+int fila_atual = 0;
 
+void SRTN () {
+    double QUANTUM;
+    int i = 0, j, contexto = 0, flag = 0;
+    clock_t begin, end;
+    double tf;
+    pthread_t run, time;
+    QUANTUM = 0.2;
+    begin = clock ();
+
+    if (d) pthread_create (&time, NULL, arrival, &begin);
+
+    while (i < size) {
+        while (1) {
+            if (listaproc[currenti].dt > 0) {
+                if ((double) ((clock () - begin)/CLOCKS_PER_SEC) >= listaproc[currenti].t0)
+                    break;
+            }
+            else currenti++;
+        }
+        flag = 0;
+
+        for (j = 0; j < size; j++) {
+            if ((listaproc[j].t0 <= (double) ((clock () - begin)/CLOCKS_PER_SEC)) && 
+                (listaproc[j].dt < listaproc[currenti].dt) && 
+                (listaproc[j].dt > 0)) {
+
+                currenti = j;
+                flag = 1;
+            }
+        }
+        if (flag) contexto++;
+        if (listaproc[currenti].dt > 0) {
+
+            if (listaproc[currenti].dt < QUANTUM) {
+                QUANTUM = listaproc[currenti].dt;
+                pthread_create (&run, NULL, lostime, &QUANTUM);
+                pthread_join (run, NULL);
+            }
+            else {
+                pthread_create (&run, NULL, lostime, &QUANTUM);
+                pthread_join (run, NULL);
+            }
+            QUANTUM = 0.2;
+
+            listaproc[currenti].dt = listaproc[currenti].dt - QUANTUM;
+
+            if (listaproc[currenti].dt <= 0) {
+                end = clock ();
+                tf = (double) (end - begin)/CLOCKS_PER_SEC;
+
+                fprintf(out, "%s %f %f\n", listaproc[currenti].nome, tf, tf - listaproc[currenti].t0);
+            
+                if (d) fprintf(stderr, "Fim do %s : %s %f %f\n", listaproc[currenti].nome, listaproc[currenti].nome, tf, tf - listaproc[currenti].t0);
+                i++;
+                currenti = 0;
+            }
+        }
+    }
+    fprintf(out, "Mudanças de Contexto: %d\n", contexto);
+    if (d) fprintf (stderr, "Terminado. Número de mudanças de Contexto: %d\n", contexto);
+
+}
+void MULTIPLAS_FILAS () {
+    int i, terminados = 0, contexto = 0;
+    double QUANTUM;
+    clock_t begin, end;
+    double tf;
+    pthread_t run, time;
+
+    filas = malloc (5 * sizeof (int *));
+    for (i = 0; i < 5; i++)
+        filas[i] = malloc (size * sizeof (int));
+    p = malloc (5 * sizeof (int));
+    u = malloc (5 * sizeof (int));
+    for (i = 0; i < 5; i++)  p[i] = u[i] = 0;
+
+    begin = clock ();
+    pthread_create (&time, NULL, arrivalM, &begin);
+    
+    while (terminados < size) {
+        if (tem_alguem (p[fila_atual], u[fila_atual])) {
+            currenti = filas[fila_atual][p[fila_atual]];
+
+            QUANTUM = (fila_atual + 1) * 0.200001;
+
+            if (listaproc[currenti].dt < QUANTUM) {
+                QUANTUM = listaproc[currenti].dt;
+                pthread_create (&run, NULL, lostime, &QUANTUM);
+                pthread_join (run, NULL);
+            }
+            else {
+                pthread_create (&run, NULL, lostime, &QUANTUM);
+                pthread_join (run, NULL);
+            }
+            listaproc[currenti].dt = listaproc[currenti].dt - QUANTUM;
+
+
+          
+
+            if (listaproc[currenti].dt > 0) {
+                contexto++;
+                if (fila_atual < 4) {
+                    p[fila_atual]++;
+                    filas[fila_atual + 1][u[fila_atual + 1]++] = currenti;
+                }
+            }
+            else {
+                end = clock ();
+                tf = (double) (end - begin)/CLOCKS_PER_SEC;
+                fprintf(out, "%s %f %f\n", listaproc[currenti].nome, tf, tf - listaproc[currenti].t0);
+                if (d) fprintf(stderr, "Fim do %s : %s %f %f\n", listaproc[currenti].nome, listaproc[currenti].nome, tf, tf - listaproc[currenti].t0);
+                p[fila_atual]++;
+                terminados++;
+            }
+
+            fila_atual = 0;
+
+        }
+        else fila_atual = (fila_atual + 1) % 5;
+
+    }
+    fprintf(out, "Mudanças de Contexto: %d\n", contexto);
+    if (d) fprintf (stderr, "Terminado. Número de mudanças de Contexto: %d\n", contexto);
+
+}
+
+void * arrivalM (void *time) {
+    int i = 0;
+    double *start = (double*)time;
+    double ops;
+
+    while (i < size) {
+        ops = (double) ((clock () - *start) / CLOCKS_PER_SEC);
+        if (ops >= listaproc[i].t0) {
+            filas[0][u[0]++] = i;
+            if (d) fprintf (stderr, "** Inicio  %s: %f %s %f %f **\n", listaproc[i].nome, listaproc[i].t0, listaproc[i].nome, listaproc[i].dt, listaproc[i].deadline);
+            i++;
+        }
+    }
+
+    return NULL;
+}
+
+int tem_alguem (int p, int u) {
+    
+    return u - p;
+}
+
+
+
+int main (int argc, char **argv) {
+    char *entrada   = argv[2];
+    char *saida     = argv[3];
+    if (argc > 4) d = 1;    
+    in = fopen (entrada, "r");
+    out = fopen (saida, "w");
+    escalonador = atoi (argv[1]);
+
+    readFile ();
+
+
+    switch (escalonador) {
+        case 1:
+            FCFS ();
+            break;
+        case 2:
+            SRTN ();
+            break;
+        case 3:
+            MULTIPLAS_FILAS ();
+            break;
+    }
+    return 0;
+}
 
 void* arrival (void *time) {
     int i = 0;
@@ -60,20 +245,20 @@ void* arrival (void *time) {
 
 void FCFS () {
     int i = 0;
-	clock_t begin, end, ops;
+    clock_t begin, end, ops;
     double tf;
     double c;
-	pthread_t *procs;
+    pthread_t *procs;
     pthread_t time;
 
 
 
-	procs = malloc (sizeof (pthread_t) * size);
+    procs = malloc (sizeof (pthread_t) * size);
     begin = clock ();
 
     if (d) pthread_create (&time, NULL, arrival, &begin);
 
-	while (i < size) {
+    while (i < size) {
             ops = (clock () - begin) / CLOCKS_PER_SEC;
             c = (double) ops;
             if (c >= listaproc[i].t0) {
@@ -89,11 +274,11 @@ void FCFS () {
             }
             else continue;
             
-	}
+    }
   
-    fprintf(out, "0\n");
+    fprintf(out, "Mudanças de Contexto: 0\n");
     if (d) fprintf (stderr, "Terminado. Número de mudanças de Contexto: 0\n");
-	    
+        
 }
 
 
@@ -112,86 +297,13 @@ void readFile () {
 
 }
 void* lostime (void *voidtime) {
-    double *time = (double *)voidtime;
+    double *time = (double *)voidtime, now;
     clock_t begin = clock ();
     cpu = sched_getcpu ();
-    if (d) fprintf (stderr, "  %s começou a usar CPU %d\n", listaproc[shortest].nome, cpu);
+    if (d) fprintf (stderr, "  %s começou a usar CPU %d\n", listaproc[currenti].nome, cpu);
 
-    while (((double) (clock () - begin)/CLOCKS_PER_SEC) < *time) {}
-    if (d-1) fprintf (stderr, "  %s terminou de usar CPU %d\n", listaproc[shortest].nome, cpu);
+    while ((now = ((double) (clock () - begin)/CLOCKS_PER_SEC)) < *time) {}
+    if (d) fprintf (stderr, "  %s terminou de usar CPU %d\n", listaproc[currenti].nome, cpu);
+
     return NULL;
-}
-
-void SRTN () {
-    double QUANTUM;
-    int i = 0, j, contexto = 0, flag = 0;
-    clock_t begin, end;
-    double tf;
-    pthread_t run, time;
-    QUANTUM = 0.1;
-    begin = clock ();
-
-    if (d) pthread_create (&time, NULL, arrival, &begin);
-
-    while (i < size) {
-        while (1) {
-            if (listaproc[shortest].dt > 0) {
-                if ((double) ((clock () - begin)/CLOCKS_PER_SEC) >= listaproc[shortest].t0)
-                    break;
-            }
-            else shortest++;
-        }
-        flag = 0;
-
-        for (j = 0; j < size; j++) {
-            if ((listaproc[j].t0 <= (double) ((clock () - begin)/CLOCKS_PER_SEC)) && 
-                (listaproc[j].dt < listaproc[shortest].dt) && 
-                (listaproc[j].dt > 0)) {
-
-                shortest = j;
-                flag = 1;
-            }
-        }
-        if (flag) contexto++;
-        if (listaproc[shortest].dt > 0) {
-            pthread_create (&run, NULL, lostime, &QUANTUM);
-            pthread_join (run, NULL);
-            listaproc[shortest].dt = listaproc[shortest].dt - QUANTUM;
-            if (listaproc[shortest].dt <= 0) {
-                end = clock ();
-                tf = (double) (end - begin)/CLOCKS_PER_SEC;
-
-                fprintf(out, "%s %f %f\n", listaproc[shortest].nome, tf, tf - listaproc[shortest].t0);
-            
-                if (d) fprintf(stderr, "Fim do %s : %s %f %f\n", listaproc[shortest].nome, listaproc[shortest].nome, tf, tf - listaproc[shortest].t0);
-                i++;
-                shortest = 0;
-            }
-        }
-    }
-    fprintf(out, "Mudanças de Contexto: %d\n", contexto);
-    if (d) fprintf (stderr, "Terminado. Número de mudanças de Contexto: %d\n", contexto);
-
-}
-
-int main (int argc, char **argv) {
-    int escalonador = atoi (argv[1]);
-    char *entrada   = argv[2];
-    char *saida     = argv[3];
-    if (argc > 4) d = 1;    
-    in = fopen (entrada, "r");
-    out = fopen (saida, "w");
-
-    readFile ();
-
-
-    switch (escalonador) {
-        case 1:
-            FCFS ();
-            break;
-        case 2:
-            SRTN ();
-            break;
-    }
-    return 0;
 }
